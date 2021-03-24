ddcecdbbaff2171687618cba0aba56e0ac39b4e5
aravind raju <aravind.raju@exponentialai.com>
2021-03-24 07:29:06 +0530

    -- updating version in requirements.txt

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -24,7 +24,7 @@ from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
-flags = tf.compat.v1.app.flags
+flags = tf.app.flags
 flags.DEFINE_string("csv_input", "", "Path to the CSV input")
 flags.DEFINE_string("output_path", "", "Path to output TFRecord")
 flags.DEFINE_string(
@@ -104,7 +104,7 @@ def create_tf_example(group, path, label_map):
 
 
 def main(_):
-    writer = tf.compat.v1.python_io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(os.getcwd(), FLAGS.img_path)
     examples = pd.read_csv(FLAGS.csv_input)
 
@@ -131,4 +131,4 @@ def main(_):
 
 
 if __name__ == "__main__":
-    tf.compat.v1.app.run()
+    tf.app.run()
